23e971995fe35d84541e5971f425f9e9a2665a17
Aamir Khan <aamirimagine@outlook.com>
2022-01-22 22:05:35 +0500

    initial commit

---
+++ src/Hubs/CollaborationHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CollaborativePdfViewer.Hubs
+{
+    public class CollaborationHub : Hub
+    {
+        public async Task OpenPdf(string path)
+        {
+            await Clients.Others.SendAsync("OnOpenPdf",path);
+        }
+        public async Task GoToNextPage(string path)
+        {
+            await Clients.Others.SendAsync("OnGoToNextPage",path);
+        }
+        public async Task GoToPreviousPage(string user)
+        {
+            await Clients.Others.SendAsync("OnGoToPreviousPage",user);
+        }
+    }
+}
